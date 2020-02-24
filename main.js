ba7f9a6b56d3f49e565f177a3c9bedffc3732563
Robert Gaggl <robert.gaggl@orf.at>
2020-02-24 09:33:52 +0100

    var -> const/let

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var {FtpServer} = require("./lib/ftpserver");
-var {Parser} = require("ringo/args");
-var term = require("ringo/term");
+const {FtpServer} = require("./lib/ftpserver");
+const {Parser} = require("ringo/args");
+const term = require("ringo/term");
 
-var parser = new Parser();
+const parser = new Parser();
 parser.addOption("p", "port", "port", "The port to listen (defaults to 2100)");
 parser.addOption(null, "keystore", "keystore", "The path to the SSL keystore");
 parser.addOption(null, "password", "password", "The SSL keystore passphrase");
@@ -10,7 +10,7 @@ parser.addOption(null, "protocol", "protocol", "The encryption protocol (ssl/tls
 parser.addOption(null, "explicitSsl", null, "Use explicit SSL encryption");
 
 
-var help = function() {
+const help = function() {
     term.writeln("Usage:");
     term.writeln("\n   ringo main.js [options] path/to/users.json\n");
     term.writeln("Available options:");
@@ -18,25 +18,25 @@ var help = function() {
     term.writeln();
 };
 
-var start = function(args) {
+const start = function(args) {
     if (args.length < 1) {
         return help();
     }
-    var opts = {};
+    const opts = {};
     try {
         parser.parse(args, opts);
     } catch (e) {
         term.writeln(term.RED, e.message, term.RESET);
         return help();
     }
-    var usersFile = args.pop();
+    const usersFile = args.pop();
     if (!usersFile) {
         term.writeln(term.RED,
                 "Please specify the JSON file containing the ftp accounts",
                 term.RESET);
         return help();
     }
-    var sslConfig = null;
+    let sslConfig = null;
     if (opts.keystore) {
         sslConfig = FtpServer.createSslConfig({
             "keystore": opts.keystore,
@@ -44,16 +44,16 @@ var start = function(args) {
             "protocol": opts.protocol || "tls"
         });
     }
-    var listener = FtpServer.createListener({
+    const listener = FtpServer.createListener({
         "port": opts.port || 2100,
         "sslConfig": sslConfig,
         "useImplicitSsl": opts.explicitSsl !== true
     });
-    var userManager = FtpServer.createUserManager(usersFile);
+    const userManager = FtpServer.createUserManager(usersFile);
     userManager.on("reloaded", function(e) { print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> reloaded users <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")});
-    var server = new FtpServer(listener, userManager);
+    const server = new FtpServer(listener, userManager);
     server.start();
-}
+};
 
 if (require.main == module.id) {
     start(require('system').args.slice(1));
